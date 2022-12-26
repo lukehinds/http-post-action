376c4e77dc88fcd106501eec3ec12b6a1d8aaf7d
Luke Hinds <lhinds@redhat.com>
2022-12-26 13:03:11 +0000

    Change from form to json payload

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import * as core from '@actions/core'
-import * as fs from 'fs'
-import * as request from 'request'
-import FormData from 'form-data'
+import request from 'request'
 
 async function run(): Promise<void> {
   try {
@@ -9,25 +7,34 @@ async function run(): Promise<void> {
     core.debug(`Posting to ${url} ...`)
 
     const artifactName: string = core.getInput('artifact_name')
-    const form = new FormData()
 
-    // form append with multipart/form-data header
-    form.append('file', fs.createReadStream(artifactName), {
-      filename: artifactName,
-      contentType: 'multipart/form-data'
-    })
-
-    const response = request.post({
+    // post artifactName as application/json to url
+    const options = {
       url,
-      body: form,
-      // use header with 'Content-Type', 'multipart/form-data'
+      method: 'POST',
       headers: {
-        ...form.getHeaders(),
-        'Content-Type': `multipart/form-data; boundary=${form.getBoundary()}`
-      }
-    })
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({artifactName})
+    }
 
-    core.debug(`Response: ${response.body}`)
+    request(
+      options,
+      (
+        error: {message: string | Error},
+        response: {statusCode: unknown; statusMessage: unknown},
+        body: unknown
+      ) => {
+        if (error) {
+          core.setFailed(error.message)
+        } else {
+          core.debug(
+            `Response: ${response.statusCode} ${response.statusMessage}`
+          )
+          core.debug(`Body: ${body}`)
+        }
+      }
+    )
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
